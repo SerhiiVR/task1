e280aa6b59a4e8458297f290c5dc7a06aa83e9a5
SerhiiVR <gorn3510@gmail.com>
2020-11-07 00:27:43 +0200

    Fixed syntax. Blanks for comments.

--- index.js
+++ index.js
@@ -5,55 +5,73 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
-
-rl.question("Enter the number: ", (lineFromUser) => {
-console.log("Result: " +  countNumbers(findMinNumber(lineFromUser), findMaxNumber(lineFromUser)));
+//
+rl.question('Enter the number: ', (lineFromUser) => {
+console.log('Result: ' +  countNumbers(findMinNumber(lineFromUser), findMaxNumber(lineFromUser)));
 rl.close();
 });
 
-
+/**
+ * 
+ * @param {*} line 
+ */
 function findMinNumber(line) {
 
     let num;
     
     for (let i = 0; i < line.length; i++) {
-    if(checkType(line[i])) {
-        num = +line[i];
-        break;
+        if (checkType(line[i])) {
+             num = +line[i];
+             break;
+        }
     }
-}
     
     for (let i = 1; i < line.length; i++) {
         if (num > +line[i]) {
             num = +line[i];
         }
     }
+
     return num;
 }
 
+/**
+ * 
+ * @param {*} line 
+ */
 function findMaxNumber(line) {
     
     let num;
     
     for (let i = 0; i < line.length; i++) {
-    if(checkType(line[i])) {
-        num = +line[i];
-        break;
+        if (checkType(line[i])) {
+            num = +line[i];
+            break;
+        }
     }
-}
     
     for (let i = 1; i < line.length; i++) {
         if (num < +line[i]) {
             num = +line[i];
         }
     }
+
     return num;
 }
 
-function countNumbers(minNum, maxNum){
+/**
+ * 
+ * @param {*} minNum 
+ * @param {*} maxNum 
+ */
+function countNumbers(minNum, maxNum) {
     return minNum + maxNum;
 }
 
-function checkType(simbol){
+/**
+ * 
+ * @param {*} simbol 
+ */
+function checkType(simbol) {
     return Number.isInteger(+simbol);
 }
