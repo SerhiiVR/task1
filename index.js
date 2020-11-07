afc15df6c80e2fb198d838f37aca9b8bcb0bdafc
SerhiiVR <gorn3510@gmail.com>
2020-11-07 10:41:35 +0200

    Final commit

--- index.js
+++ index.js
@@ -5,53 +5,42 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
-//
+const MAX_NUMBER = 9;
+const MIN_NUMBER = 0;
+
+// Input data from the user via the command line.
 rl.question('Enter the number: ', (lineFromUser) => {
 console.log('Result: ' +  countNumbers(findMinNumber(lineFromUser), findMaxNumber(lineFromUser)));
 rl.close();
 });
 
 /**
- * 
- * @param {*} line 
+ * The function takes a string as input and finds the minimum digit in it.
+ * @param {*} line line of numbers.
  */
 function findMinNumber(line) {
 
-    let num;
-    
+    let num = MAX_NUMBER;
+    // Comparison of numbers in a string. Search for the minimum number from them.
     for (let i = 0; i < line.length; i++) {
-        if (checkType(line[i])) {
-             num = +line[i];
-             break;
-        }
-    }
-    
-    for (let i = 1; i < line.length; i++) {
-        if (num > +line[i]) {
+        if (num > +line[i] & checkType(line[i])) {
             num = +line[i];
         }
     }
-
+    
     return num;
 }
 
 /**
- * 
- * @param {*} line 
+ * The function takes a string as input and finds the maximum digit in it.
+ * @param {*} line стока чисел.
  */
 function findMaxNumber(line) {
     
-    let num;
-    
+    let num = MIN_NUMBER;
+    // Comparison of numbers in a string. Search for the maximum number from them.
     for (let i = 0; i < line.length; i++) {
-        if (checkType(line[i])) {
-            num = +line[i];
-            break;
-        }
-    }
-    
-    for (let i = 1; i < line.length; i++) {
-        if (num < +line[i]) {
+        if (num < +line[i] & checkType(line[i])) {
             num = +line[i];
         }
     }
@@ -60,18 +49,20 @@ function findMaxNumber(line) {
 }
 
 /**
- * 
- * @param {*} minNum 
- * @param {*} maxNum 
+ * The function adds two numbers and returns the result of the operation.
+ * @param {*} minNum first number.
+ * @param {*} maxNum second number.
  */
 function countNumbers(minNum, maxNum) {
     return minNum + maxNum;
 }
 
 /**
- * 
- * @param {*} symbol 
+ * The function takes a symbol as input and checks that it is exactly a number.
+ * @param {*} symbol symbol.
  */
 function checkType(symbol) {
+    if(symbol !== ' ') {
     return Number.isInteger(+symbol);
+    }
 }
